dd3201427d81fbaa4471f35287d9d22d00bd44e0
cszatma <christopherszatmary@gmail.com>
2018-03-30 18:09:12 -0400

    Added mongoose and connected it to a MongoDB instance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 // @flow
 
 import express from 'express';
+import mongoose from 'mongoose';
 
 import './services/passport';
 import authRoutes from './routes/authRoutes';
+import keys from './config/keys';
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
