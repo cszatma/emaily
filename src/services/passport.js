5010ae5396576dea2b7a7e9268bd434f8f929627
cszatma <christopherszatmary@gmail.com>
2018-03-31 17:24:40 -0400

    Added ability to save users to the database when they first log in, added functions to passport for serializing and deserializing users

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,9 +1,21 @@
+// @flow
+
 import passport from 'passport';
 import PassportGoogleStrategy from 'passport-google-oauth20';
+import mongoose from 'mongoose';
 
 import keys from '../config/keys';
 
 const GoogleStrategy = PassportGoogleStrategy.Strategy;
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+});
 
 passport.use(
     new GoogleStrategy(
@@ -12,8 +24,24 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback',
         },
-        (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
+        (
+            accessToken: string,
+            refreshToken: string,
+            profile: { id: string },
+            done: (?Error, any) => any,
+        ) => {
+            User.findOne({ googleId: profile.id })
+                .then(existingUser => {
+                    if (existingUser) {
+                        done(null, existingUser);
+                    } else {
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user))
+                            .catch(error => console.log(error));
+                    }
+                })
+                .catch(error => console.log(error));
         },
     ),
 );
