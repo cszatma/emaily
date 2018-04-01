3c22b830d3bdb843e8dfcebb80ceabcd36d1e43d
cszatma <christopherszatmary@gmail.com>
2018-04-01 14:23:41 -0400

    Added route handlers to authRoutes for logging out and getting the current user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,8 @@
 // @flow
 
 import passport from 'passport';
-import type { $Application } from 'express';
+import type { $Application, $Response } from 'express';
+import type { session$Request } from '../utils/types';
 
 export default (app: $Application) => {
     app.get(
@@ -12,4 +13,13 @@ export default (app: $Application) => {
     );
 
     app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/logout', (req: session$Request, res: $Response) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req: session$Request, res: $Response) => {
+        res.send(req.user);
+    });
 };
