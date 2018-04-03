74de795356fdc5490a2af72191df80e63d852673
cszatma <christopherszatmary@gmail.com>
2018-04-03 10:54:18 -0400

    Setup redux store in index.js, fixed import paths of moved files

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import 'materialize-css/dist/css/materialize.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './styles/index.scss';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
 registerServiceWorker();
