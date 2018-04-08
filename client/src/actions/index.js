d96e6cfaa3b9ca5b80aaeea8eeb2371ddbc10a25
cszatma <christopherszatmary@gmail.com>
2018-04-08 17:35:56 -0400

    Added handleToken() action creator which sends the stripe token to the backend and then re-fetches the user, cleaned up fetchUser() using fetchUserAction()

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,10 +2,18 @@
 
 import axios from 'axios';
 
-import { FETCH_USER } from './actionTypes';
+import { fetchUserAction } from './actionTypes';
 import type { ThunkAction, Dispatch } from 'actions';
+import type { StripeToken } from 'redux-extensions';
 
 export const fetchUser = (): ThunkAction => async (dispatch: Dispatch): any => {
     const res = await axios.get('/api/current_user');
-    dispatch({ type: FETCH_USER, payload: res.data });
+    dispatch(fetchUserAction(res.data));
+};
+
+export const handleToken = (token: StripeToken): ThunkAction => async (
+    dispatch: Dispatch,
+) => {
+    const res = await axios.post('/api/stripe', token);
+    dispatch(fetchUserAction(res.data));
 };
