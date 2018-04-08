78e36afcfee8885f80e0aea2b241535da0d19239
cszatma <christopherszatmary@gmail.com>
2018-04-07 22:11:32 -0400

    Completed Header component, connected it to the redux store

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,55 @@
 // @flow
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-export default class Header extends Component<{}> {
+import type { AuthState, ReduxState } from 'redux-extensions';
+
+type Props = {
+    auth: AuthState,
+};
+
+class Header extends Component<Props, {}> {
+    renderContent() {
+        console.log(this.props.auth);
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login With Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
+        }
+    }
+
     render() {
         return (
-            <div>
-                <h2>Header</h2>
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
+                        Emaily
+                    </Link>
+                    <ul className="right">{this.renderContent()}</ul>
+                </div>
+            </nav>
         );
     }
 }
+
+function mapStateToProps({ auth }: ReduxState) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
