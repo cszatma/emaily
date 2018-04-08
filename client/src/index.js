46ef6974f71c7c1128184d188e883adf56f6101f
cszatma <christopherszatmary@gmail.com>
2018-04-07 22:13:11 -0400

    Passed reduxThunk to applyMiddleware() when creating the redux store

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import 'materialize-css/dist/css/materialize.min.css';
 
 import './styles/index.scss';
@@ -9,7 +10,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}>
