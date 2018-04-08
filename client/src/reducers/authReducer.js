722ee4912f41dc980030c465a1b3ecbb103e1152
cszatma <christopherszatmary@gmail.com>
2018-04-07 22:10:56 -0400

    Added FETCH_USER case to authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,13 @@
 // @flow
 
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/actionTypes';
+import type { Action } from 'actions';
+import type { AuthState } from 'redux-extensions';
+
+export default function(state: AuthState = null, action: Action): AuthState {
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false;
         default:
             return state;
     }
