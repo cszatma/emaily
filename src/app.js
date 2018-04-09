30d133fda93e46769827b1aebf2a461b47ebfc90
cszatma <christopherszatmary@gmail.com>
2018-04-09 08:32:49 -0400

    Moved app into its own file app.js to allow it to easily be tested independently

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import passport from 'passport/lib/index';
+import bodyParser from 'body-parser';
+import express from 'express';
+import cookieSession from 'cookie-session';
+
+import keys from './config/keys';
+import authRoutes from './routes/authRoutes';
+import billingRoutes from './routes/billingRoutes';
+
+const app = express();
+
+// Setup global middlewares that all routes will use
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 2592000000, // 30 * 24 * 60 * 60 * 1000 = 30 days
+        keys: [keys.cookieKey],
+    }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Setup route handlers
+authRoutes(app);
+billingRoutes(app);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,17 @@
 // @flow
 
-import express from 'express';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import passport from 'passport';
-import bodyParser from 'body-parser';
 import chalk from 'chalk';
 
-import authRoutes from './routes/authRoutes';
-import billingRoutes from './routes/billingRoutes';
 import keys from './config/keys';
 import './models/User';
 import './services/passport';
 import dev from './utils/dev-utils';
+import app from './app';
 
 mongoose.connect(keys.mongoURI);
 
-const app = express();
-
-// Setup global middlewares that all routes will use
-app.use(bodyParser.json());
-app.use(
-    cookieSession({
-        maxAge: 2592000000, // 30 * 24 * 60 * 60 * 1000 = 30 days
-        keys: [keys.cookieKey],
-    }),
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Setup route handlers
-authRoutes(app);
-billingRoutes(app);
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
-
-dev.log(chalk.green(`Server successfully started on port ${PORT}\n`));
+app.listen(PORT, () => {
+    dev.log(chalk.green(`Server successfully started on port ${PORT}\n`));
+});
