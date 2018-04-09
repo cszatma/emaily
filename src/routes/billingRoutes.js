3d2f46117da4381ec0f56ae1ee49a75caf00b2b7
cszatma <christopherszatmary@gmail.com>
2018-04-08 20:02:05 -0400

    Used requireLogin middleware in billingRoutes, updated the credits of the user and saved the changes to the database after the charge was completed

--- src/routes/billingRoutes.js
+++ src/routes/billingRoutes.js
@@ -5,22 +5,26 @@ import type { $Application, $Response, NextFunction } from 'express';
 import type { billing$Request, StripeToken } from 'emaily-types';
 
 import keys from '../config/keys';
+import requireLogin from '../middlewares/requireLogin';
 
 const stripe = stripeApi(keys.stripeSecretKey);
 
 export default (app: $Application) => {
     app.post(
         '/api/stripe',
+        requireLogin,
         async (req: billing$Request, res: $Response, next: NextFunction) => {
+            // Verify that a body was passed and that it's and object
             if (!req.body || typeof req.body !== 'object') {
-                res
-                    .status(500)
-                    .send(
-                        'Invalid body in request. Expected an object representing a stripe response token.',
-                    );
+                res.status(400).send({
+                    error:
+                        'Invalid body in request.' +
+                        'Expected an object representing a stripe response token.',
+                });
                 return next();
             }
 
+            // Create a charge then update the user model
             const charge = await stripe.charges.create({
                 amount: 500,
                 currency: 'usd',
@@ -28,7 +32,10 @@ export default (app: $Application) => {
                 source: req.body.id,
             });
 
-            console.log(charge);
+            req.user.credits += 5;
+            const user = await req.user.save();
+
+            res.send(user);
         },
     );
 };
