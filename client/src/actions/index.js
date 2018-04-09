4185f7e4c1c8d1eeefa2c8367eb2f40e77da4e58
cszatma <christopherszatmary@gmail.com>
2018-04-08 22:25:07 -0400

    Refactored actions in actions/index.js to resolve issue with jest

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,14 +6,16 @@ import { fetchUserAction } from './actionTypes';
 import type { ThunkAction, Dispatch } from 'actions';
 import type { StripeToken } from 'redux-extensions';
 
-export const fetchUser = (): ThunkAction => async (dispatch: Dispatch): any => {
-    const res = await axios.get('/api/current_user');
-    dispatch(fetchUserAction(res.data));
-};
+export function fetchUser(): ThunkAction {
+    return async (dispatch: Dispatch): any => {
+        const res = await axios.get('/api/current_user');
+        dispatch(fetchUserAction(res.data));
+    };
+}
 
-export const handleToken = (token: StripeToken): ThunkAction => async (
-    dispatch: Dispatch,
-) => {
-    const res = await axios.post('/api/stripe', token);
-    dispatch(fetchUserAction(res.data));
-};
+export function handleToken(token: StripeToken): ThunkAction {
+    return async (dispatch: Dispatch): any => {
+        const res = await axios.post('/api/stripe', token);
+        dispatch(fetchUserAction(res.data));
+    };
+}
