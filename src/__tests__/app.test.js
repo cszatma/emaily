a15046c1c20a75399acc32a238250f63a2d7f79a
cszatma <christopherszatmary@gmail.com>
2018-04-18 15:30:48 -0400

    Added tests for authRoutes using passport-mock-strategy

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,12 +1,25 @@
-import request from 'supertest';
+import Agent from 'jest-supertest-cookie-fix';
+import passport from 'passport';
+import {
+    mockUser,
+    MockStrategy,
+    setupSerializeAndDeserialize,
+} from 'passport-mock-strategy';
 
 import app from '../app';
+
+setupSerializeAndDeserialize(passport);
+passport.use(new MockStrategy());
+
+app.get('/auth/mock', passport.authenticate('mock'), (req, res) => {
+    res.send({ status: 'ok' });
+});
 
 describe('Express app tests', () => {
     let agent;
 
     beforeEach(() => {
-        agent = request.agent(app);
+        agent = Agent(app);
     });
 
     it('should respond to a get on /api/current_user', () => {
@@ -14,6 +27,33 @@ describe('Express app tests', () => {
     });
 
     it('should redirect to /', () => {
-        return agent.get('/api/logout').expect(302).expect('Location', '/');
+        return agent
+            .get('/api/logout')
+            .expect(302)
+            .expect('Location', '/');
+    });
+
+    it('should authenticate the user', () => {
+        return agent.get('/auth/mock').expect(200);
+    });
+
+    it('should return the an empty object since a user is not logged in', () => {
+        return agent.get('/api/current_user').expect(200, {});
+    });
+
+    describe('Test authenticated routes', () => {
+        beforeEach(() => {
+            return agent.get('/auth/mock');
+        });
+
+        it('should return the current user', () => {
+            return agent.get('/api/current_user').expect(200, mockUser);
+        });
+
+        it('should log out the user', () => {
+            return agent
+                .get('/api/logout')
+                .then(() => agent.get('/api/current_user').expect(200, {}));
+        });
     });
 });
