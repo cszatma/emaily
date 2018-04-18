49ab46e239000f8038ec7ea2e8671bb0bb01ede0
cszatma <christopherszatmary@gmail.com>
2018-04-18 15:37:36 -0400

    Changed ...options from an optional parameter in log() in dev-utils.js

--- src/utils/dev-utils.js
+++ src/utils/dev-utils.js
@@ -7,7 +7,7 @@
  * @param message The message to log.
  * @param options Any additional options to be passed.
  */
-function log(message: string, ...options?: any[]) {
+function log(message: string, ...options: any[]) {
     if (process.env.NODE_ENV !== 'production') {
         console.log(message, options && options.length > 0 ? options : '');
     }
