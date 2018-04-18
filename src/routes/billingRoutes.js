545b544e77396355b18b3a393e2f1ff4f0aa66c8
cszatma <christopherszatmary@gmail.com>
2018-04-18 15:30:06 -0400

    Removed unused type and variable in billingRoutes

--- src/routes/billingRoutes.js
+++ src/routes/billingRoutes.js
@@ -2,7 +2,7 @@
 
 import stripeApi from 'stripe';
 import type { $Application, $Response, NextFunction } from 'express';
-import type { billing$Request, StripeToken } from 'emaily-types';
+import type { billing$Request } from 'emaily-types';
 
 import keys from '../config/keys';
 import requireLogin from '../middlewares/requireLogin';
@@ -25,7 +25,7 @@ export default (app: $Application) => {
             }
 
             // Create a charge then update the user model
-            const charge = await stripe.charges.create({
+            await stripe.charges.create({
                 amount: 500,
                 currency: 'usd',
                 description: '$5 for 5 credits',
