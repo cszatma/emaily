9d2e7434c572fbc21a4f686e1f7e9f8284e83e74
cszatma <christopherszatmary@gmail.com>
2018-04-19 18:46:18 -0400

    Added Survey and Recipient models

---
+++ src/models/Survey.js
@@ -0,0 +1,30 @@
+// @flow
+
+import mongoose from 'mongoose';
+
+import RecipientSchema from './Recipient';
+
+const { Schema } = mongoose;
+
+const SurveySchema = new Schema({
+    title: String,
+    body: String,
+    subject: String,
+    recipients: [RecipientSchema],
+    yes: {
+        type: Number,
+        default: 0,
+    },
+    no: {
+        type: Number,
+        default: 0,
+    },
+    _user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    dateSent: Date,
+    lastResponded: Date,
+});
+
+mongoose.model('surveys', SurveySchema);
