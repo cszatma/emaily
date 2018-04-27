fce4cf2e51d141de855d3123ed5fd3af14c7ec3a
cszatma <christopherszatmary@gmail.com>
2018-04-27 18:34:26 -0400

    Added SurveyField which renders a form input and label, added the formReducer to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 // @flow
 
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer';
 
 export default combineReducers({
     auth: authReducer,
+    form: formReducer,
 });
