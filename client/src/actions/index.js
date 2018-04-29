a83cab936659adc730842f7d30888a99444b82d8
cszatma <christopherszatmary@gmail.com>
2018-04-29 11:58:03 -0400

    Added action to submit survey

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,12 @@
 // @flow
 
 import axios from 'axios';
+import type { RouterHistory } from 'react-router-dom';
 
 import { fetchUserAction } from './actionTypes';
 import type { ThunkAction, Dispatch } from 'actions';
 import type { StripeToken } from 'redux-extensions';
+import type { FormValues } from '../components/surveys/formFields';
 
 export function fetchUser(): ThunkAction {
     return async (dispatch: Dispatch): any => {
@@ -16,6 +18,17 @@ export function fetchUser(): ThunkAction {
 export function handleToken(token: StripeToken): ThunkAction {
     return async (dispatch: Dispatch): any => {
         const res = await axios.post('/api/stripe', token);
+        dispatch(fetchUserAction(res.data));
+    };
+}
+
+export function submitSurvey(
+    values: FormValues,
+    history: RouterHistory,
+): ThunkAction {
+    return async (dispatch: Dispatch): any => {
+        const res = await axios.post('/api/surveys', values);
+        history.push('/surveys');
         dispatch(fetchUserAction(res.data));
     };
 }
